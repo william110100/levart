f932497e835534259b11d025fbbe90dce8fc5746
William <williammicrosoft0810@gmail.com>
2024-08-21 11:17:11 +0700

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+"use client";
+
+import { JOB } from "@/utils/constants";
+import { parseDate } from "@internationalized/date";
+import { FileInput } from "@mantine/core";
+import { Button, Checkbox, DatePicker, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Radio, RadioGroup, Select, SelectItem, useDisclosure } from "@nextui-org/react";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+  name: string;
+  email: string;
+  password: string;
+  gender: string;
+  dateOfBirth: string;
+  cvFile: any;
+  job: string;
+  confirmPass: string;
+  agreement: boolean;
+}
 
 export default function Home() {
+  const { handleSubmit, register, reset, setValue, control, getValues, formState: {errors, isValid} } = useForm<IFormInput>({
+    defaultValues: { email: "", password: "", confirmPass: "", name: "", agreement: false, dateOfBirth: "", gender: "male", cvFile: ""},
+    mode: "onChange",
+  });
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+  const onSubmitForm = (data: IFormInput) => {
+    console.log(data)
+    if (data?.confirmPass !== data?.password) {
+      setShowAlert(true);
+    } else {
+      onOpen();
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <form
+        className="flex flex-col gap-y-2"
+        onSubmit={handleSubmit(onSubmitForm)}
+      >
+        <Input
+          {...register("name", { required: "Name is required" })}
+          label="Name"
+          placeholder="Enter your name"
+          type="text"
+          isRequired
+          errorMessage={errors?.name?.message}
+          isInvalid={errors?.name ? true : false}
+          onChange={(e) => setValue("name", e.target.value)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <Input
+          {...register("email", {
+            required: "Email must be filled",
+            pattern: /^[a-zA-Z0-9.-]+(.[a-zA-Z]{2,})+/,
+          })}
+          label="Email"
+          placeholder="Enter your email"
+          type="text"
+          isRequired
+          errorMessage={errors?.email?.message}
+          isInvalid={errors?.email ? true : false}
+          onChange={(e) => setValue("email", e.target.value)}
+        />
+        <DatePicker isRequired label="Date of Birth" onChange={(e) => setValue("dateOfBirth", e?.toString() ?? "")} showMonthAndYearPickers />
+        <div className="flex gap-x-2">
+          <RadioGroup label="Select your gender" isRequired onValueChange={(value) => setValue("gender", value)} defaultValue={getValues("gender")}>
+            <Radio value="male">Male</Radio>
+            <Radio value="female">Female</Radio>
+          </RadioGroup>
+        </div>
+        <Select isRequired label="Job" onChange={(e) => setValue("job", e.target.value)} placeholder="Select you job" items={JOB}>{(job: any) => (<SelectItem key={job.key}>{job.label}</SelectItem>)}</Select>
+        {/* <FileInput label="Attach your CV" required error={<span className="text-red-500 text-md leading-5">{errors?.cvFile ? errors?.cvFile?.message : ""}</span>} onChange={(e) => setValue("cvFile", e)} placeholder="Your CV" withAsterisk /> */}
+        <Input
+          {...register("password", {
+            required: "Password must be fill",
+            pattern:
+              /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+          })}
+          label="Password"
+          placeholder="Enter your Password"
+          type="password"
+          errorMessage={errors?.password?.message}
+          isInvalid={errors?.password ? true : false}
+          isRequired
+        />
+        <Input
+          {...register("confirmPass", {
+            required: "Confirm must be fill",
+            pattern:
+              /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+          })}
+          label="Confirm Password"
+          placeholder="Repeat your password"
+          type="password"
+          isRequired
+          errorMessage={errors?.confirmPass?.message}
+          isInvalid={errors?.confirmPass ? true : false}
+        />
+        <Checkbox isRequired onValueChange={(isSelected) => setValue("agreement", isSelected)}>Agreement</Checkbox>
+        <Button type="submit">Submit</Button>
+      </form>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+        {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Isi Form Berhasil</ModalHeader>
+              <ModalBody>
+                <p>Anda bisa tutup pop up ini</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={() => {
+                  onClose();
+                  reset();
+                }}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </main>
-  )
+  );
 }
---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+export const JOB = [
+  {key: "designer", label: "Designer"},
+  {key: "programmer", label: "Programmer"},
+  {key: "sales", label: "Sales"},
+  {key: "finance", label: "Finance"},
+]
